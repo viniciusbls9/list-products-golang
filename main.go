d06a1571499e0fc9a0061cb5d882e0f1f56f7f12
viniciusbls9 <beneditoloura@gmail.com>
2023-08-30 08:11:57 -0300

    feat: using func in main.go

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"database/sql"
-	"net/http"
-
+	"github.com/labstack/echo/v4"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -13,27 +11,8 @@ type Product struct {
 }
 
 func main() {
-	http.HandleFunc("/", hello)
-	http.ListenAndServe(":8080", nil)
-}
-
-func hello(w http.ResponseWriter, r *http.Request) {
-	product := Product{Name: "Iphone", Price: 1000}
-	persistProduct(product)
-	w.Write([]byte("Hello World"))
-}
-
-func persistProduct(product Product) {
-	db, err := sql.Open("sqlite3", "test.db")
-	if err != nil {
-		panic(err)
-	}
-
-	defer db.Close()
-	stmt, err := db.Prepare("Insert into products(name, price) values($1, $2)")
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = stmt.Exec(product.Name, product.Price)
+	e := echo.New()
+	e.GET("/product", handlerListProducts)
+	e.POST("/product", handlerCreateProduct)
+	e.Logger.Fatal(e.Start(":8080"))
 }
